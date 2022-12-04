af4ec402469dd7cc22d412bf94f734d640329a49
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-05 00:20:50 +0500

    dopz1

--- dopz/Program.cs
+++ dopz/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿//Задача из темы циклов
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int max1 = 0;
+int max2 = 0;
+while (n != 0)
+{    
+    if (n > max1)
+    {
+        max2 = max1;
+        max1 = n;
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+}
+Console.WriteLine($"второе максимальное = {max2}");
