6715b0edb15ff537b253929349d38ff16e8e3204
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-04 17:40:03 +0500

    10z_last

--- 10z/Program.cs
+++ 10z/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int n = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерированное число: {n}");
+int n12 = n / 10;
+int n2 = n12 % 10;
+Console.WriteLine($"вторая цифра числа: {n2}");
