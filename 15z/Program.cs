653b0deeedc4237f0944446352c218bf5503306d
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-04 23:18:12 +0500

    15z_last

--- 15z/Program.cs
+++ 15z/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите цифру дня недели от 1 до 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 1 || n == 2 || n == 3 || n == 4 || n == 5)
+Console.WriteLine("Рабочий день");
+else if (n == 6 || n == 7)
+Console.WriteLine("Выходной");
