e8c9809312307a2898cc23358783e2fe8f7c53f1
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-05 15:44:40 +0500

    13z_konechnaya

--- 13z/Program.cs
+++ 13z/Program.cs
@@ -3,55 +3,12 @@ Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 if (n <= -100 || n >= 100)
 {
-    if (n <= -1000 || n >= 1000)
+    while (n < -1000 || n > 1000)
     {
-        if (n <= -10000 || n >= 10000)
-        {
-            if (n <= -100000 || n >= 100000)
-            {
-                if (n <= -1000000 || n >= 1000000)
-                {
-                    if (n <= -10000000 || n >= 10000000)
-                    {
-                        if (n <= -1000000000 || n >= 1000000000)
-                        {
-                            n = n / 1000000;
-                            Console.WriteLine($"Третья цифра числа: {n % 10}");                                             
-                        }
-                        else
-                        {
-                            n = n / 100000;
-                            Console.WriteLine($"Третья цифра числа: {n % 10}");
-                        }
-                    }
-                    else
-                    {
-                        n = n / 10000;
-                        Console.WriteLine($"Третья цифра числа: {n % 10}");
-                    }
-                }
-                else
-                {
-                    n = n / 1000;
-                    Console.WriteLine($"Третья цифра числа: {n % 10}");
-                }
-            }
-            else
-            {
-                n = n / 100;
-                Console.WriteLine($"Третья цифра числа: {n % 10}");
-            }
-        }
-        else
-        {
-            n = n / 10;
-            Console.WriteLine($"Третья цифра числа: {n % 10}");
-        }
-    }                 
-    else
-    {
-        Console.WriteLine($"Третья цифра числа: {n % 10}");
+        n = n / 10;
     }
+    n = n % 10;
+    Console.WriteLine($"Третья цифра числа = {n}");
 }
 else
 {
